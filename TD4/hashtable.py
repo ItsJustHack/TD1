c0ad3c1a8f51c2796631aef5a43bb0732eb7fa4d
Eliott Leboeuf <eliott.leboeuf@gmail.com>
2024-04-03 11:30:57 +0200

    Solutions up to exercise 5

--- TD4/hashtable.py
+++ TD4/hashtable.py
@@ -2,28 +2,84 @@ INITIAL_LENGTH = 10
 
 class Hashtable:
 
-    def __init__(self, hash_function):
+    def __init__(self, hash_function, length = INITIAL_LENGTH):
         self.__hash_function = hash_function
-        self.__length = INITIAL_LENGTH # Useful if we want to increase N
+        self.__length = length # Useful if we want to increase N
         self.__table = []
         for i in range(self.__length):
             self.__table.append([])
 
-    def __str__(self):
-        return str(self.__table)
 
+    @property
+    def table(self):
+        """The table property."""
+        return self.__table
+    @table.setter
+    def table(self, value):
+        self.__table = value
+
+    @property
+    def length(self):
+        """The length property."""
+        return self.__length
+
+    @length.setter
+    def length(self, value):
+        self.__length = value
+
+    def __str__(self):
+        """Returns the table as a string"""
+        return str(self.table)
+
+    def keys_pos(self, pos):
+        """Returns the keys at position pos"""
+        return [key for (key, value) in self.__table[pos]]
+
+    def values_pos(self, pos):
+        """Returns the values at position pos"""
+        return [key for (key, value) in self.__table[pos]]
 
     def put(self, key, value): 
-        pos = self.__hash_function(key) % self.__length
-        if (key, value) not in self.__table[pos]:
-            self.__table[pos].append((key, value))
+        """Add a value to the hashing table"""
+        pos = self.__hash_function(key) % self.length
+        if (key, value) not in self.table[pos]:
+            self.table[pos].append((key, value))
         else : 
-            # truc immonde mais flemme de réfléchir
-            i = [key for (key, value) in self.__table[pos]].index(key)
-            self.__table[pos][i] = (key, value)
+            i = self.keys_pos(pos).index(key)
+            self.table[pos][i] = (key, value)
+    
+    def get(self, key):
+        """Get a value from a key"""
+        pos = self.__hash_function(key) % self.length
+        keys = self.keys_pos(pos) 
+        if key not in keys: 
+            return None
+        else:
+            return self.table[pos][keys.index(key)][1]
+
+    def repartition(self):
+        """Print the repartition of collisions"""
+        import matplotlib.pyplot as plt
+        y = [len(self.keys_pos(pos)) for pos in range(self.length)]
+        x = range(self.length)
+        width = 1/1.5
+        plt.bar(x, y, width, color="blue")
+        plt.show()
+
+    def resize(self):
+        self.length *= 2
+        
 
 
 
 def first_hash_function(x : str):
     """ Create the first naïve hash function"""
     return sum([ord(char) for char in x])
+
+
+def second_hash_function(x : str): 
+    """ Create a better hash function"""
+    h = 0
+    for (i, char) in enumerate(x):
+        h = 31*h + ord(char)
+    return h
--- TD4/test_hashtable.py
+++ TD4/test_hashtable.py
@@ -1,23 +1,54 @@
 import unittest 
 from hashtable import Hashtable 
 from hashtable import first_hash_function 
+from hashtable import second_hash_function 
 
 class TestHashtable(unittest.TestCase):
     def setUp(self):
         self.hash_test = Hashtable(first_hash_function)
-
-    def test_create_hash(self):
-        self.assertEqual(first_hash_function("abc"), 294)
-
-    def test_put(self):
         self.hash_test.put("abc", 1)
         self.hash_test.put("ab", 2)
         self.hash_test.put("a", 3)
         self.hash_test.put("abcd", 4)
-        self.hash_test.put("abce", 5)
-        self.hash_test.put("abcef", 6)
-        print(self.hash_test)
-        
+        self.hash_test.put("abcde", 5)
+        self.hash_test.put("abcdef", 6)
+
+    def test_create_hash(self):
+        self.assertEqual(first_hash_function("abc"), 294)
+
+    def test_get(self):
+        self.assertEqual(self.hash_test.get("abc"), 1)
+        self.assertEqual(self.hash_test.get("b"), None)
+        self.assertEqual(self.hash_test.get("ab"), 2)
+        self.assertEqual(self.hash_test.get("a"), 3)
+        self.assertEqual(self.hash_test.get("abcd"), 4)
+        self.assertEqual(self.hash_test.get("abcde"), 5)
+        self.assertEqual(self.hash_test.get("abcdef"), 6)
+
+    def test_repartition(self):
+        self.hash_test.repartition()
+
+    def test_french_dictionnaire_first_hash(self):
+        try:
+            h = Hashtable(first_hash_function, 320)
+            f = open("frenchssaccent.dic")
+            for ligne in f:
+                mot = ligne.rstrip("\n")
+                h.put(mot, len(mot))
+            h.repartition()
+        except Exception as e:
+            raise e
+
+    def test_french_dictionnaire_second_hash(self):
+        try:
+            h = Hashtable(second_hash_function, 320)
+            f = open("frenchssaccent.dic")
+            for ligne in f:
+                mot = ligne.rstrip("\n")
+                h.put(mot, len(mot))
+            h.repartition()
+        except Exception as e:
+            raise e
         
 
 if __name__ == "__main__":
