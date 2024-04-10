aec378ee61610a18e716126d888f248b04b66f1c
Eliott Leboeuf <eliott.leboeuf@gmail.com>
2024-04-10 10:40:57 +0200

    Création de la cible, du bouton quitter et feu

---
+++ TD5/target.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+
+class Target:
+    def __init__(self):
+        
+        def create_circle(x, y, r, color = "red"): #center coordinates, radius
+            x0 = x - r
+            y0 = y - r
+            x1 = x + r
+            y1 = y + r
+            return self.__canvas.create_oval(x0, y0, x1, y1, outline = color)
+
+        def feu():
+            pass
+
+        def draw_target():
+            # Efface tout contenu précédent
+            self.__canvas.delete("all")
+            
+            self.__canvas.create_oval(200 - 170, 200 - 170, 200 + 170, 200 +
+                                    170, outline="red", fill="white")
+
+            self.__canvas.create_oval(200 - 50, 200 - 50, 200 + 50, 200 + 50,
+                                   outline="red", fill="red")
+
+            self.__canvas.create_oval(200 - 20, 200 - 20, 200 + 20, 200 + 20,
+                                   outline="red", fill="white")
+            # Dessine les cercles concentriques
+            for i in range(5):
+                #color = "red" if i != 4 else "white"
+                #self.__canvas.create_oval(200 - 30*(i+1), 200 - 30*(i+1), 200 + 30*(i+1), 200 + 30*(i+1), outline="red", fill=color)
+                create_circle(200, 200, 200 - 30*(i))
+            
+            # Dessine les axes
+            self.__canvas.create_line(200, 0, 200, 400, fill="red", width=1)
+            self.__canvas.create_line(0, 200, 400, 200, fill="red", width=1)
+            
+            # Dessine les chiffres
+            n = 7
+            for i in range(1, n):
+                color = "red" if i != 2 else "white"
+                self.__canvas.create_text(200, 200 - 30*(i-1), text=str(n-i), fill=color, font=("Helvetica", 14))
+
+        # Création de la fenêtre principale
+        self.__root = tk.Tk()
+        self.__root.title("Cible")
+
+        # Création du Canvas
+        self.__canvas = tk.Canvas(self.__root, width=400, height=400, bg = "red")
+        self.__canvas.pack()
+
+        # Bouton "Dessiner"
+        draw_target()
+
+        # Bouton "Quitter"
+        quit_button = tk.Button(self.__root, text="Quitter", command=self.__root.destroy)
+        quit_button.pack(side=tk.RIGHT)
+
+        fire_button = tk.Button(self.__root, text="Feu !", command=feu)
+        fire_button.pack(side=tk.LEFT)
+
+    def execute(self):
+        self.__root.mainloop()
+
+if __name__ == "__main__":
+    target = Target()
+    target.execute()
+
